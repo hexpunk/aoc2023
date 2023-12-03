147bdf751e76eb9981dee9aaab3cce738ecdebba
Jay Sherby <10983817+hexpunk@users.noreply.github.com>
2023-12-03 18:11:34 +0000

    Day 3, Part 1 complete

--- 03/solution.ts
+++ 03/solution.ts
@@ -1,34 +1,91 @@
 import readline from "readline";
-import { parseLine, hasAdjacentSymbol, Part, Symbol } from "./shared";
+
+interface Token {
+  line: number;
+  column: number;
+}
+
+interface Sym extends Token {}
+
+interface Num extends Token {
+  value: string;
+}
+
+interface State {
+  line: number;
+  nums: Num[];
+  syms: Sym[];
+}
+
+// Modifies `state`
+function parseLine(state: State, input: string) {
+  const re = /\d+|[^\.]/dg;
+  let match;
+
+  while ((match = re.exec(input)) !== null) {
+    const literal = match[0];
+    const column = match.indices![0][0];
+
+    const token: Token = { line: state.line, column };
+
+    if (literal[0] >= "0" && literal[0] <= "9") {
+      state.nums.push({ ...token, value: literal });
+    } else {
+      state.syms.push(token as Sym);
+    }
+  }
+
+  state.line++;
+}
+
+function isPart(num: Num, syms: Sym[]): boolean {
+  // Assumes `state.syms` is already sorted
+  for (const sym of syms) {
+    if (sym.line < num.line - 1) {
+      continue;
+    }
+
+    if (sym.line > num.line + 1) {
+      break;
+    }
+
+    if (
+      sym.line === num.line &&
+      (sym.column === num.column - 1 ||
+        sym.column === num.column + num.value.length)
+    ) {
+      return true;
+    }
+
+    if (
+      sym.line !== num.line &&
+      sym.column >= num.column - 1 &&
+      sym.column <= num.column + num.value.length
+    ) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 const rl = readline.createInterface({
   input: process.stdin,
   terminal: false,
 });
 
-let currentLine = 1;
-const allSymbols: Symbol[] = [];
-const allParts: Part[] = [];
-
+const state: State = { line: 1, syms: [], nums: [] };
 rl.on("line", (line) => {
-  const { symbols, parts } = parseLine(currentLine, line);
-  allSymbols.push(...symbols);
-  allParts.push(...parts);
-  currentLine++;
+  parseLine(state, line);
 });
 
 rl.on("close", () => {
-  const partNumbers = new Set<number>();
-  for (const part of allParts) {
-    console.log(part.partNumber, hasAdjacentSymbol(part, allSymbols));
-    if (hasAdjacentSymbol(part, allSymbols)) {
-      partNumbers.add(Number(part.partNumber));
+  let total = 0;
+  for (const num of state.nums) {
+    if (isPart(num, state.syms)) {
+      total += Number(num.value);
     }
   }
 
-  const partNumberTotal = Array.of(...partNumbers.values()).reduce(
-    (total, num) => total + num
-  );
-
-  console.log(`Part 1 total: ${partNumberTotal}`);
+  console.log(`Part 1 total: ${total}`);
 });
