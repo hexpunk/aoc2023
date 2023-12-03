482bcb678a8a81d5828201fc44f0a1c658954d09
Jay Sherby <10983817+hexpunk@users.noreply.github.com>
2023-12-03 18:29:59 +0000

    Day 3 complete

--- 03/solution.ts
+++ 03/solution.ts
@@ -3,18 +3,13 @@ import readline from "readline";
 interface Token {
   line: number;
   column: number;
-}
-
-interface Sym extends Token {}
-
-interface Num extends Token {
   value: string;
 }
 
 interface State {
   line: number;
-  nums: Num[];
-  syms: Sym[];
+  nums: Token[];
+  syms: Token[];
 }
 
 // Modifies `state`
@@ -26,20 +21,20 @@ function parseLine(state: State, input: string) {
     const literal = match[0];
     const column = match.indices![0][0];
 
-    const token: Token = { line: state.line, column };
+    const token: Token = { line: state.line, column, value: literal };
 
     if (literal[0] >= "0" && literal[0] <= "9") {
-      state.nums.push({ ...token, value: literal });
+      state.nums.push(token);
     } else {
-      state.syms.push(token as Sym);
+      state.syms.push(token);
     }
   }
 
   state.line++;
 }
 
-function isPart(num: Num, syms: Sym[]): boolean {
-  // Assumes `state.syms` is already sorted
+function isPart(num: Token, syms: Token[]): boolean {
+  // Assumes `syms` is already sorted
   for (const sym of syms) {
     if (sym.line < num.line - 1) {
       continue;
@@ -69,6 +64,31 @@ function isPart(num: Num, syms: Sym[]): boolean {
   return false;
 }
 
+function gearRatio(sym: Token, nums: Token[]): number | null {
+  if (sym.value !== "*") {
+    return null;
+  }
+
+  const parts: Token[] = [];
+
+  // Assumes `nums` is already sorted
+  for (const num of nums) {
+    if (parts.length > 2) {
+      return null;
+    }
+
+    if (isPart(num, [sym])) {
+      parts.push(num);
+    }
+  }
+
+  if (parts.length !== 2) {
+    return null;
+  }
+
+  return Number(parts[0].value) * Number(parts[1].value);
+}
+
 const rl = readline.createInterface({
   input: process.stdin,
   terminal: false,
@@ -80,12 +100,18 @@ rl.on("line", (line) => {
 });
 
 rl.on("close", () => {
-  let total = 0;
+  let partNumberTotal = 0;
   for (const num of state.nums) {
     if (isPart(num, state.syms)) {
-      total += Number(num.value);
+      partNumberTotal += Number(num.value);
     }
   }
 
-  console.log(`Part 1 total: ${total}`);
+  let gearRatioTotal = 0;
+  for (const sym of state.syms) {
+    gearRatioTotal += gearRatio(sym, state.nums) ?? 0;
+  }
+
+  console.log(`Part 1 total: ${partNumberTotal}`);
+  console.log(`Part 2 total: ${gearRatioTotal}`);
 });
