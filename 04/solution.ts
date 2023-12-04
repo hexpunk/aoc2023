481153146b632411a56f0631ba14d7086ef7fa95
Jay Sherby <10983817+hexpunk@users.noreply.github.com>
2023-12-04 18:04:19 +0000

    Day 4 complete

--- 04/solution.ts
+++ 04/solution.ts
@@ -1,19 +1,16 @@
 import readline from "readline";
 
-function parseCard(input: string): number {
+function getMatchCount(input: string): number {
   // Replace pesky double or triple spaces
   input = input.replaceAll(/ {2,}/g, " ");
 
-  const { groups } = /^Card (?<card>.*): (?<winners>.*) \| (?<picks>.*)$/g.exec(
-    input
-  )!;
+  const { groups } = /^Card .*: (?<winners>.*) \| (?<picks>.*)$/g.exec(input)!;
 
   const winners = new Set<string>(groups!.winners.split(" "));
 
   const matches = groups!.picks.split(" ").filter((pick) => winners.has(pick));
 
-  // Total points
-  return matches.length > 0 ? Math.pow(2, matches.length - 1) : 0;
+  return matches.length;
 }
 
 const rl = readline.createInterface({
@@ -21,11 +18,30 @@ const rl = readline.createInterface({
   terminal: false,
 });
 
+let currentCardIndex = 0;
 let pointsTotal = 0;
+let numCards: number[] = [];
+
 rl.on("line", (line) => {
-  pointsTotal += parseCard(line);
+  numCards[currentCardIndex] = numCards[currentCardIndex] ?? 1;
+
+  const matches = getMatchCount(line);
+
+  if (matches > 0) {
+    pointsTotal += Math.pow(2, matches - 1);
+  }
+
+  for (let i = 1; i <= matches; i++) {
+    numCards[currentCardIndex + i] = numCards[currentCardIndex + i] ?? 1;
+    numCards[currentCardIndex + i] += numCards[currentCardIndex];
+  }
+
+  currentCardIndex++;
 });
 
 rl.on("close", () => {
   console.log(`Part 1 total: ${pointsTotal}`);
+  console.log(
+    `Part 2 total: ${numCards.reduce((total, num) => total + num, 0)}`
+  );
 });
