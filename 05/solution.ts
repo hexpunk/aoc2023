4ce334cabe7b6a31b03d84c4504ead5851113d80
Jay Sherby <10983817+hexpunk@users.noreply.github.com>
2023-12-06 01:00:05 +0000

    Day 5 Part 2 complete

--- 05/solution.ts
+++ 05/solution.ts
@@ -1,16 +1,12 @@
 import readline from "readline";
 
-interface Seed {
-  id: number;
-}
-
 interface Mapping {
   destinationRangeStart: number;
   sourceRangeStart: number;
   rangeLength: number;
 }
 
-const seeds: Seed[] = [];
+const part1Seeds: number[] = [];
 const seedToSoil: Mapping[] = [];
 const soilToFertilizer: Mapping[] = [];
 const fertilizerToWater: Mapping[] = [];
@@ -20,32 +16,26 @@ const temperatureToHumidity: Mapping[] = [];
 const humidityToLocation: Mapping[] = [];
 
 function parseSeeds(input: string) {
-  seeds.push(
-    ...input
-      .substring("seeds: ".length)
-      .split(" ")
-      .map((id) => ({
-        id: Number(id),
-      }))
-  );
+  part1Seeds.push(...input.substring("seeds: ".length).split(" ").map(Number));
 }
 
-let currentMap: Mapping[] | undefined = undefined;
+let currentMap: Mapping[] | null = null;
 function parseMap(input: string) {
   if (input.trim().length === 0) {
     // Sort by source range start to make lookup more efficient
     currentMap?.sort((a, b) => a.sourceRangeStart - b.sourceRangeStart);
-    currentMap = undefined;
+    currentMap = null;
   } else if (input.endsWith("map:")) {
-    currentMap = {
-      "seed-to-soil": seedToSoil,
-      "soil-to-fertilizer": soilToFertilizer,
-      "fertilizer-to-water": fertilizerToWater,
-      "water-to-light": waterToLight,
-      "light-to-temperature": lightToTemperature,
-      "temperature-to-humidity": temperatureToHumidity,
-      "humidity-to-location": humidityToLocation,
-    }[input.split(" ")[0]];
+    currentMap =
+      {
+        "seed-to-soil": seedToSoil,
+        "soil-to-fertilizer": soilToFertilizer,
+        "fertilizer-to-water": fertilizerToWater,
+        "water-to-light": waterToLight,
+        "light-to-temperature": lightToTemperature,
+        "temperature-to-humidity": temperatureToHumidity,
+        "humidity-to-location": humidityToLocation,
+      }[input.split(" ")[0]] ?? null;
 
     if (typeof currentMap === "undefined") {
       throw new Error(`Couldn't find mapping for "${input}"`);
@@ -83,7 +73,23 @@ function getDestination(mappings: Mapping[], sourceId: number): number {
   }
 }
 
-function seedIdToLocationId({ id: seedId }: Seed): number {
+function getSource(mappings: Mapping[], destinationId: number): number {
+  const mapping = mappings.find(
+    (mapping) =>
+      mapping.destinationRangeStart <= destinationId &&
+      mapping.destinationRangeStart + mapping.rangeLength - 1 >= destinationId
+  );
+
+  if (typeof mapping === "undefined") {
+    return destinationId;
+  } else {
+    const offset = destinationId - mapping.destinationRangeStart;
+
+    return mapping.sourceRangeStart + offset;
+  }
+}
+
+function seedToLocationId(seedId: number): number {
   const soilId = getDestination(seedToSoil, seedId);
   const fertilizerId = getDestination(soilToFertilizer, soilId);
   const waterId = getDestination(fertilizerToWater, fertilizerId);
@@ -92,6 +98,17 @@ function seedIdToLocationId({ id: seedId }: Seed): number {
   const humidityId = getDestination(temperatureToHumidity, temperatureId);
 
   return getDestination(humidityToLocation, humidityId);
+}
+
+function locationToSeedId(locationId: number): number {
+  const humidityId = getSource(humidityToLocation, locationId);
+  const temperatureId = getSource(temperatureToHumidity, humidityId);
+  const lightId = getSource(lightToTemperature, temperatureId);
+  const waterId = getSource(waterToLight, lightId);
+  const fertilizerId = getSource(fertilizerToWater, waterId);
+  const soilId = getSource(soilToFertilizer, fertilizerId);
+
+  return getSource(seedToSoil, soilId);
 }
 
 const rl = readline.createInterface({
@@ -104,7 +121,28 @@ rl.on("line", (line) => {
 });
 
 rl.on("close", () => {
-  const locationIds = seeds.map(seedIdToLocationId);
+  let lowestLocationId: number | null = null;
 
-  console.log(`Part 1 lowest location number: ${Math.min(...locationIds)}`);
+  for (const seed of part1Seeds) {
+    lowestLocationId = Math.min(
+      lowestLocationId ?? Infinity,
+      seedToLocationId(seed)
+    );
+  }
+
+  console.log(`Part 1 lowest location number: ${lowestLocationId}`);
+
+  for (let i = 0; ; i++) {
+    const seedId = locationToSeedId(i);
+
+    for (let j = 0; j < part1Seeds.length; j += 2) {
+      const start = part1Seeds[j];
+      const length = part1Seeds[j + 1];
+
+      if (seedId >= start && seedId <= start + length) {
+        console.log(`Part 2 lowest location number: ${i}`);
+        return;
+      }
+    }
+  }
 });
