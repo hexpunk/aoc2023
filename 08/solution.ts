441fb6a7922247cd04270570e302de0b832a141e
Jay Sherby <10983817+hexpunk@users.noreply.github.com>
2023-12-08 21:53:41 +0000

    Day 8 Part 2 complete

--- 08/solution.ts
+++ 08/solution.ts
@@ -6,14 +6,26 @@ interface Node {
 }
 
 let path: string | null = null;
-const nodes = new Map<string, Node>();
+const nodeMap = new Map<string, Node>();
 
 function parseNode(input: string) {
-  const [, id, left, right] = /^([A-Z]{3}) = \(([A-Z]{3}), ([A-Z]{3})\)$/g.exec(
-    input
-  )!;
+  const [, id, left, right] = /^(.{3}) = \((.{3}), (.{3})\)$/g.exec(input)!;
 
-  nodes.set(id, { left, right });
+  nodeMap.set(id, { left, right });
+}
+
+function gcd(a: number, b: number): number {
+  while (b > 0) {
+    const t = a;
+    a = b;
+    b = t % b;
+  }
+
+  return a;
+}
+
+function lcm(a: number, b: number): number {
+  return (a / gcd(a, b)) * b;
 }
 
 const rl = readline.createInterface({
@@ -34,7 +46,7 @@ rl.on("close", () => {
   let currentNodeName = "AAA";
 
   while (currentNodeName !== "ZZZ") {
-    const node = nodes.get(currentNodeName)!;
+    const node = nodeMap.get(currentNodeName)!;
     const nextDirection = path!.charAt(steps % path!.length);
     steps++;
 
@@ -42,4 +54,25 @@ rl.on("close", () => {
   }
 
   console.log(`Part 1 total steps: ${steps}`);
+
+  const nodeSteps = Array.from(nodeMap.keys())
+    .filter((name) => name.charAt(2) === "A")
+    .map((name) => {
+      let steps = 0;
+      let currentNodeName = name;
+
+      while (currentNodeName.charAt(2) !== "Z") {
+        const node = nodeMap.get(currentNodeName)!;
+        const nextDirection = path!.charAt(steps % path!.length);
+        steps++;
+
+        currentNodeName = nextDirection === "L" ? node.left : node.right;
+      }
+
+      return steps;
+    });
+
+  steps = nodeSteps.reduce((prev, curr) => lcm(prev, curr));
+
+  console.log(`Part 2 total steps: ${steps}`);
 });
