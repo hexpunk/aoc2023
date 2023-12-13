51769fcfa73f2a3217997b33fce1d6871908ea21
Jay Sherby <10983817+hexpunk@users.noreply.github.com>
2023-12-13 04:10:00 +0000

    Day 12 Part 2 complete

--- 12/solution.ts
+++ 12/solution.ts
@@ -1,40 +1,54 @@
 import readline from "readline";
 
+const cache = new Map<string, number>();
 function countSolutions(puzzle: string, runs: number[]): number {
-  if (puzzle === "") {
+  function calc(puzzle: string, runs: number[]): number {
+    if (puzzle === "") {
+      if (runs.length === 0) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+
     if (runs.length === 0) {
-      return 1;
-    } else {
-      return 0;
+      if (puzzle.indexOf("#") === -1) {
+        return 1;
+      } else {
+        return 0;
+      }
     }
-  }
 
-  if (runs.length === 0) {
-    if (puzzle.indexOf("#") === -1) {
-      return 1;
-    } else {
-      return 0;
+    const [first, ...rest] = runs;
+    let subtotal = 0;
+
+    if (puzzle[0] === "." || puzzle[0] === "?") {
+      subtotal += countSolutions(puzzle.substring(1), runs);
     }
-  }
 
-  const [first, ...rest] = runs;
-  let subtotal = 0;
-
-  if (puzzle[0] === "." || puzzle[0] === "?") {
-    subtotal += countSolutions(puzzle.substring(1), runs);
-  }
-
-  if (puzzle[0] === "#" || puzzle[0] === "?") {
-    if (
-      first <= puzzle.length &&
-      puzzle.substring(0, first).indexOf(".") === -1 &&
-      (first === puzzle.length || puzzle[first] !== "#")
-    ) {
-      subtotal += countSolutions(puzzle.substring(first + 1), rest);
+    if (puzzle[0] === "#" || puzzle[0] === "?") {
+      if (
+        first <= puzzle.length &&
+        puzzle.substring(0, first).indexOf(".") === -1 &&
+        (first === puzzle.length || puzzle[first] !== "#")
+      ) {
+        subtotal += countSolutions(puzzle.substring(first + 1), rest);
+      }
     }
+
+    return subtotal;
   }
 
-  return subtotal;
+  const key = puzzle + "|" + String(runs);
+
+  if (cache.has(key)) {
+    return cache.get(key)!;
+  } else {
+    const result = calc(puzzle, runs);
+    cache.set(key, result);
+
+    return result;
+  }
 }
 
 const rl = readline.createInterface({
@@ -42,17 +56,24 @@ const rl = readline.createInterface({
   terminal: false,
 });
 
-let sum = 0;
-let count = 0;
+let sum1 = 0;
+let sum2 = 0;
 
 rl.on("line", (line) => {
-  count++;
-  console.log(`Running on line ${count}`);
   const [puzzle, rawNumbers] = line.split(" ");
+  const numbers = rawNumbers.split(",").map(Number);
 
-  sum += countSolutions(puzzle, rawNumbers.split(",").map(Number));
+  sum1 += countSolutions(puzzle, numbers);
+  sum2 += countSolutions([puzzle, puzzle, puzzle, puzzle, puzzle].join("?"), [
+    ...numbers,
+    ...numbers,
+    ...numbers,
+    ...numbers,
+    ...numbers,
+  ]);
 });
 
 rl.on("close", () => {
-  console.log(`Part 1 sum: ${sum}`);
+  console.log(`Part 1 sum: ${sum1}`);
+  console.log(`Part 2 sum: ${sum2}`);
 });
