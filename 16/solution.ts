4d0582df0a6a60c696468345dae48db585306860
Jay Sherby <10983817+hexpunk@users.noreply.github.com>
2023-12-17 05:14:55 +0000

    Day 16 Part 1 complete

---
+++ 16/solution.ts
@@ -0,0 +1,161 @@
+import readline from "readline";
+
+enum Direction {
+  North = "N",
+  West = "W",
+  South = "S",
+  East = "E",
+}
+
+interface Beam {
+  line: number;
+  column: number;
+  direction: Direction;
+}
+
+interface State {
+  visited: Set<string>;
+  activeBeams: Beam[];
+}
+
+function isDone(state: State): boolean {
+  return state.activeBeams.length === 0;
+}
+
+function newState(): State {
+  return {
+    visited: new Set(),
+    activeBeams: [{ line: 0, column: -1, direction: Direction.East }],
+  };
+}
+
+function beamToKey(beam: Beam): string {
+  return `${beam.line},${beam.column}|${beam.direction}`;
+}
+
+function beamOutOfBounds(grid: string[][], beam: Beam): boolean {
+  return (
+    beam.line < 0 ||
+    beam.column < 0 ||
+    beam.line >= grid.length ||
+    beam.column >= grid[beam.line].length
+  );
+}
+
+function beamVisited(state: State, beam: Beam): boolean {
+  return state.visited.has(beamToKey(beam));
+}
+
+function stepBeam(beam: Beam) {
+  if (beam.direction === Direction.North) {
+    beam.line--;
+  } else if (beam.direction === Direction.West) {
+    beam.column--;
+  } else if (beam.direction === Direction.South) {
+    beam.line++;
+  } else if (beam.direction === Direction.East) {
+    beam.column++;
+  }
+}
+
+function runStep(grid: string[][], state: State, beam: Beam) {
+  stepBeam(beam);
+
+  if (beamOutOfBounds(grid, beam) || beamVisited(state, beam)) {
+    state.activeBeams = state.activeBeams.filter((b) => b !== beam);
+  } else {
+    state.visited.add(beamToKey(beam));
+
+    const char = grid[beam.line][beam.column];
+
+    if (char === "\\") {
+      switch (beam.direction) {
+        case Direction.North:
+          beam.direction = Direction.West;
+          break;
+
+        case Direction.West:
+          beam.direction = Direction.North;
+          break;
+
+        case Direction.South:
+          beam.direction = Direction.East;
+          break;
+
+        case Direction.East:
+          beam.direction = Direction.South;
+          break;
+      }
+    } else if (char === "/") {
+      switch (beam.direction) {
+        case Direction.North:
+          beam.direction = Direction.East;
+          break;
+
+        case Direction.West:
+          beam.direction = Direction.South;
+          break;
+
+        case Direction.South:
+          beam.direction = Direction.West;
+          break;
+
+        case Direction.East:
+          beam.direction = Direction.North;
+          break;
+      }
+    } else if (
+      char === "|" &&
+      (beam.direction === Direction.East || beam.direction === Direction.West)
+    ) {
+      beam.direction = Direction.North;
+      state.activeBeams.push({ ...beam, direction: Direction.South });
+    } else if (
+      char === "-" &&
+      (beam.direction === Direction.North || beam.direction === Direction.South)
+    ) {
+      beam.direction = Direction.East;
+      state.activeBeams.push({ ...beam, direction: Direction.West });
+    }
+  }
+}
+
+function simulate(grid: string[][], state: State) {
+  while (!isDone(state)) {
+    for (const beam of state.activeBeams) {
+      runStep(grid, state, beam);
+    }
+  }
+}
+
+function countBeamedSpaces(state: State): number {
+  const spaces = new Set<string>();
+
+  for (const key of Array.from(state.visited)) {
+    const [coords] = key.split("|");
+    spaces.add(coords);
+  }
+
+  return spaces.size;
+}
+
+const grid: string[][] = [];
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  terminal: false,
+});
+
+rl.on("line", (line) => {
+  grid.push(line.split(""));
+});
+
+rl.on("close", () => {
+  const state = newState();
+
+  simulate(grid, state);
+
+  const count = countBeamedSpaces(state);
+
+  console.log(`Part 1 total spaces: ${count}`);
+});
